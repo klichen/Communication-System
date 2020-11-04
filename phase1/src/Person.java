83152b459691487a734e7705afd343e1d4e22926
chowkyra <kyra.chow@mail.utoronto.ca>
2020-11-04 16:22:06 -0500

    "beginning of implementation of Person.java"

--- phase1/src/Person.java
+++ phase1/src/Person.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class Person {
+    private String username;
+    private String password;
+    private ArrayList<Person> contactList;
+
+    public Person(String username, String password){
+        this.username = username;
+        this.password = password;
+        this.contactList = new ArrayList<>();
+    }
+
+    public Person getTypePerson(){
+        return this;
+    }
+
+    public ArrayList<Person> getContactList(){
+        return this.contactList;
+    }
+
+    public void setContactList(ArrayList<Person> contactList) {
+        this.contactList = contactList;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
