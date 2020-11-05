968c58c041f7e00707307661dd82edbab1b8f9e2
ayalajua <juan.ayala@mail.utoronto.ca>
2020-11-04 21:37:01 -0500

    AttendeeManager, OrganizerManager, and SpeakerManager classes made

---
+++ phase1/src/SpeakerManager.java
@@ -0,0 +1,7 @@
+public class SpeakerManager {
+    //  Log in
+
+    //  see a list of talks that they are giving.
+
+    //  Message all Attendees who are signed up for a particular event, at once or individually.
+}
