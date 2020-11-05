95c8c49e17d34f4f696eab371c957f4ff3fbee68
chowkyra <kyra.chow@mail.utoronto.ca>
2020-11-04 22:19:32 -0500

    "implementation of Person.java"

--- phase1/src/Attendee.java
+++ phase1/src/Attendee.java
@@ -1,17 +1,19 @@
 import java.util.ArrayList;
 
-public class Attendee extends Person{
+public class Attendee extends Person {
+
     public Attendee(String username, String password) {
         super(username, password);
+        contactList = new ArrayList<Person>();
     }
 
     @Override
     ArrayList<Person> getContactList() {
-        return null;
+        return this.contactList;
     }
 
     @Override
     void addToContactList(Person contact) {
-
+        this.contactList.add(contact);
     }
 }
--- phase1/src/Person.java
+++ phase1/src/Person.java
@@ -1,16 +1,14 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public abstract class Person<Public> {
+    protected ArrayList<Person> contactList;
     private String username;
     private String password;
-    private ArrayList<Person> contactList;
     private ArrayList<String> messageStorage;
 
     public Person(String username, String password){
         this.username = username;
         this.password = password;
-        this.contactList = new ArrayList<>();
         this.messageStorage = new ArrayList<>();
     }
 
