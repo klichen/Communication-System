d96e33002edb31fe92962008b38536929d4d4423
chowkyra <kyra.chow@mail.utoronto.ca>
2020-11-10 14:52:46 -0500

    "implementation of SpeakerManager.java"

--- phase1/src/OrganizerManager.java
+++ phase1/src/OrganizerManager.java
@@ -1,9 +1,26 @@
 public class OrganizerManager {
+
+    private Organizer organizer;
+
+    public OrganizerManager(Organizer organizer){
+        this.organizer = organizer;
+    }
+
     // Log in
 
     // Enter rooms into the system (events)
+    public void addRoomToSystem(){
+
+    }
 
     // Create speaker accounts
+    public void createSpeaker(String username, String password){
+        Speaker speaker = new Speaker(username, password);
+    }
 
     // Schedule the speakers
+    // need to know how event is set up
+    public void scheduleSpeakers(){
+
+    }
 }
--- phase1/src/SpeakerManager.java
+++ phase1/src/SpeakerManager.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
+
 public class SpeakerManager {
+
+    private Speaker speaker;
+
+    public SpeakerManager(Speaker speaker){
+        this.speaker = speaker;
+    }
     //  Log in
 
     //  see a list of talks that they are giving.
+    public ArrayList<Event> getSchedule(){
+        return speaker.getSchedule();
+    }
 
     //  Message all Attendees who are signed up for a particular event, at once or individually.
 }
