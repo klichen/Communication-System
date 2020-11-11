cbd415450a2f896bc3fa8e0841c4829a8731edc1
Curtain64 <bryanbui2504@gmail.com>
2020-11-10 19:40:23 -0500

    "implementation of EventScheduler"

--- phase1/src/EventScheduler.java
+++ phase1/src/EventScheduler.java
@@ -1,7 +1,20 @@
 
 public class EventScheduler {
-    private Event[] lst;
-    public EventScheduler(Event event) {
-
+    private Event[] eventList;
+    public EventScheduler(Event[] eventList) {
+        this.eventList = eventList;
+    }
+    public boolean checkEventList() {
+        int i = 0;
+        while (i < this.eventList.length){
+            for (int j = i; j < this.eventList.length; j++) {
+                if (this.eventList[j].getTime() <= this.eventList[i].getTime() &
+                        this.eventList[i].getTime() < this.eventList[j].getTime() + 100 & i != j) {
+                    return false;
+                }
+            }
+            i++;
+        }
+        return true;
     }
 }
