0ed528f742afe0fbcd35eb011c4ac1a4335b8ced
chowkyra <kyra.chow@mail.utoronto.ca>
2020-11-10 19:05:01 -0500

    "adjustment of Event.java"

--- phase1/src/Event.java
+++ phase1/src/Event.java
@@ -1,20 +1,25 @@
-package src;
 
 public class Event {
     private int person;
     private int time;
-    private int speaker_id;
-    public Event(int _id, int time, int speaker){
+    private Person speaker;
+    public Event(int _id, int time, Person speaker){
         this.person = _id;
         this.time = time;
-        this.speaker_id = speaker;
+        this.speaker = speaker;
     }
     // getter for time
     public int getTime() {
         return this.time;
     }
+
     // getter for speaker_id
-    public int getSpeaker_id() {
-        return this.speaker_id;
+    public String getSpeaker_id() {
+        return this.speaker.getUsername();
+    }
+
+    //getter for speaker
+    public Person getSpeaker(){
+        return this.speaker;
     }
 }
--- phase1/src/EventScheduler.java
+++ phase1/src/EventScheduler.java
@@ -1,4 +1,3 @@
-package src;
 
 public class EventScheduler {
     private Event[] lst;
