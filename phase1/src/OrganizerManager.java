44369206da8b26bcea8033a56f4dfdfccff54db7
ayalajua <juan.ayala@mail.utoronto.ca>
2020-11-13 20:40:44 -0500

    Implemented SpeakerSystem, simplified SpeakerManager, and added method to create organizer accounts to OrganizerManager.

--- phase1/src/OrganizerManager.java
+++ phase1/src/OrganizerManager.java
@@ -1,5 +1,26 @@
+import com.sun.org.apache.xpath.internal.operations.Or;
+
 import java.util.ArrayList;
 import java.util.Map;
 
 public class OrganizerManager {
+    private ArrayList<Organizer> allOrganizers;
+    private Map<String, Organizer> usernameToOrganizer;
+
+    // Get map (username -> Organizer object)
+    public Map<String, Organizer> getUsernameToOrganizer() {
+        return usernameToOrganizer;
+    }
+
+    // Create organizer accounts
+    public void createOrganizer(String username, String password){
+        Organizer organizer = new Organizer(username, password);
+        allOrganizers.add(organizer);
+        usernameToOrganizer.put(username, organizer);
+    }
+
+    // Get all organizers
+    public ArrayList<Organizer> getAllOrganizers(){
+        return allOrganizers;
+    }
 }
--- phase1/src/SpeakerSystem.java
+++ phase1/src/SpeakerSystem.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpeakerSystem {
     // Controller class (gets input from user)
+    SpeakerManager sm;
+
+    // Get list of talks they are giving (schedule)
+    public ArrayList<String> getSchedule(String speakerUsername){
+        return sm.getSchedule(speakerUsername);
+    }
 }
