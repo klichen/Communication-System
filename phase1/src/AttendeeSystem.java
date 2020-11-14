157f9ceaf2e7d8d2c7a040a8106fae42c586077a
chowkyra <kyra.chow@mail.utoronto.ca>
2020-11-13 21:29:55 -0500

    implemented AttendeeSystem and made some changes to Attendee, and AttendeeSystem

--- phase1/src/AttendeeSystem.java
+++ phase1/src/AttendeeSystem.java
@@ -1,3 +1,38 @@
+import java.util.ArrayList;
+
 public class AttendeeSystem {
     // Controller class (gets input from user)
+
+    AttendeeManager am;
+
+    public AttendeeSystem(AttendeeManager am){
+        this.am = am;
+    }
+
+    // can attendee create an account
+    public boolean canCreateAccount(String username){
+        return this.am.canCreateAccount(username);
+    }
+
+    // create attendee account
+    public void createAccount(String username, String password){
+        this.am.createAccount(username, password);
+    }
+
+    // can attendee add a contact
+    public boolean canAddContact(String username, String contact){
+        return this.am.canAddToContactList(username, contact);
+    }
+
+    // add a contact
+    public void addContact(String username, String contact){
+        this.am.addToContactList(username, contact);
+    }
+
+    // get attendee Schedule
+    // will change once eventScheduler map is created
+    public ArrayList<String> getSchedule(String username){
+        return this.am.getSchedule(username);
+    }
+
 }
