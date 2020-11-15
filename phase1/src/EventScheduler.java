c7ab1f500f0f95f24c75ec65d8d411df1ed8d926
Curtain64 <bryanbui2504@gmail.com>
2020-11-14 19:12:52 -0500

    "adjusments to EventScheduler"

--- phase1/src/EventScheduler.java
+++ phase1/src/EventScheduler.java
@@ -5,13 +5,13 @@ import java.util.Map;
 
 public class EventScheduler {
 
-    private List<Event> ListOfEvents;
+    private List<String> ListOfStrings;
     private Map<String, Event> idToEvent;
 
     // Constructor for EventScheduler
     // Contains the main list of events
     public EventScheduler(){
-        ListOfEvents =  new ArrayList<Event>();
+        ListOfStrings =  new ArrayList<String>();
         idToEvent = new HashMap<>();
     }
 
@@ -19,7 +19,7 @@ public class EventScheduler {
     public boolean updateEvents(String roomNum, String id, int time, String speakerUsername, String speakerPassword){
         if(validEvent(roomNum, id, time, speakerUsername)){
             Event s = new Event(roomNum, id, time, speakerUsername, speakerPassword);
-            ListOfEvents.add(s);
+            ListOfStrings.add(id);
             idToEvent.put(id, s);
             System.out.println(idToEvent);
             return true;
@@ -29,8 +29,8 @@ public class EventScheduler {
 
     // Removes an event from list of events and idToEvent
     public void removeEvent(String id){
-        if (ListOfEvents.contains(id)){
-            ListOfEvents.remove(id);
+        if (ListOfStrings.contains(id)){
+            ListOfStrings.remove(id);
             idToEvent.remove(id);
         }
     }
@@ -45,37 +45,37 @@ public class EventScheduler {
         if(time < 9 || time > 16){
             return false;
         }
-        for(Event i : ListOfEvents){
+        for(String i : ListOfStrings){
             // Check for double booking room
-            if(time == i.getTime() && roomNum.equals(i.getRoomNum())){
+            if(time == idToEvent.get(i).getTime() && roomNum.equals(idToEvent.get(i).getRoomNum())){
                 return false;
             }
             // ID for each event must be unique
-            if(id.equals(i.getID())){
+            if(id.equals(idToEvent.get(i).getID())){
                 return false;
             }
             // Check for speaker at two places at same time
-            if(time == i.getTime() &&
-                    speakerUsername.equals(i.getSpeaker())){
+            if(time == idToEvent.get(i).getTime() &&
+                    speakerUsername.equals(idToEvent.get(i).getSpeaker())){
                 return false;
             }
         }
         return true;
     }
 
-    // Returns the main list of events
-    public List<Event> getEventList(){
-        return ListOfEvents;
+    // Returns the main list of event ids
+    public List<String> getEventList(){
+        return ListOfStrings;
     }
 
     // Converts the list of events into a List<List<String>>
     // in order to be saved to txt file
     public List<List<String>> eventToString(){
         List<List<String>> convertedEvents = new ArrayList<List<String>>();
-        for (Event i : ListOfEvents){
+        for (String i : ListOfStrings){
             List<String> tempConvert = new ArrayList<String>();
-            tempConvert.add(i.getRoomNum() + ","+ i.getID() + "," + i.getTime() + ","
-                    + i.getSpeaker() + ","+ i.getSpeakerPassword());
+            tempConvert.add(idToEvent.get(i).getRoomNum() + ","+ i + "," + idToEvent.get(i).getTime() + ","
+                    + idToEvent.get(i).getSpeaker() + ","+ idToEvent.get(i).getSpeakerPassword());
             convertedEvents.add(tempConvert);
         }
         return convertedEvents;
