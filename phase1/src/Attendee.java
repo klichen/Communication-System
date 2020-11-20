01ff9bad2d64edf3ec27af984649c305a95d4d48
chowkyra <kyra.chow@mail.utoronto.ca>
2020-11-19 20:02:09 -0500

    Added Javadoc to Attendee and made it Lists instead of ArrayList

--- phase1/src/Attendee.java
+++ phase1/src/Attendee.java
@@ -1,9 +1,10 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Attendee extends Person{
-    private ArrayList<String> contactList;
-    private ArrayList<String> schedule;
+    private List<String> contactList;
+    private List<String> schedule;
 
 
     public Attendee(String username, String password) {
@@ -15,23 +16,42 @@ public class Attendee extends Person{
         this.schedule = new ArrayList<>();
     }
 
-    // returns the list of people the Person can message
-    ArrayList<String> getContactList() {
+    /**
+     * returns the usernames of all the people that the Attendee could can message
+     * @return An ArrayList containing each contacts username as a String
+     */
+    public List<String> getContactList() {
         return this.contactList;
     }
 
+    /**
+     * Adds another persons username to Attendee's contactList
+     * @param contact The username of the person the Attendee wants to add to their contactList
+     */
     public void addToContact(String contact){
         this.contactList.add(contact);
     }
 
-    public ArrayList<String> getSchedule(){
+    /**
+     * returns the ids of all the events that the Attendee has signed up for
+     * @return An Arraylist containing the event ids as a String
+     */
+    public List<String> getSchedule(){
         return this.schedule;
     }
 
+    /**
+     * Adds an event's ID to the Attendees Schedule
+     * @param eventID The ID of the event the Attendee is adding to their schedule
+     */
     public void addToSchedule(String eventID){
         this.schedule.add(eventID);
     }
 
+    /**
+     * Removes an event's ID from the Attendee's Schedule
+     * @param eventID The ID of the event the Attendee is removing from their schedule
+     */
     public void removeFromSchedule(String eventID){
         this.schedule.remove(eventID);
     }
